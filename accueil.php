752bb81a11f0c0236d2f8220b59385ddfd47db4e
Lendraxae <alexandre.girolt@sfr.fr>
2022-05-11 15:12:37 +0200

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -3,11 +3,30 @@
     <head>
         <meta charset="utf-8">
         <title>Accueil</title>
-        <link href="file.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+          crossorigin="anonymous">
+       <!-- Mon css -->
+       <link href="style.css" rel="stylesheet">
+
+          
+        
    </head>
    <body>
-        <h2>Hello</h2>
-        https://github.com/donSel/mini_projet_2022.git
-        <script src="script.js"></script>
+       
+    <div class="container-fluid bg-primary">
+        <nav class="navbar navbar-dark">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="accueil.php">
+                <img src="caduceus-2029254_640.webp" width="50" height="60">
+                здоровье
+            </a>
+            <div class="barre">
+                <button class="btn btn-outline-light" type="submit" onclick="window.location.href = 'connect.php';">Se connecter</button>
+            </div>
+            </div>
+        </nav>
+    </div>
    </body>
 </html>
--- connect.php
+++ connect.php
@@ -2,45 +2,28 @@
 <html lang=fr>
     <head>
         <meta charset=utf-8>
-        <title></title>
+        <title>Se connecter</title>
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+          crossorigin="anonymous">
+       <!-- Mon css -->
+       <link href="style.css" rel="stylesheet">
     </head>
+    
     <body>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <div class="container-fluid bg-primary">
+        <nav class="navbar navbar-dark">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="accueil.php">
+                <img src="caduceus-2029254_640.webp" width="50" height="60">
+                здоровье
+            </a>
+            <div class="barre">
+                <button class="btn btn-outline-light" type="submit" onclick="window.location.href = 'connect.php';">Se connecter</button>
+            </div>
+            </div>
+        </nav>
     </div>
-  </div>
-</nav>
     </body>
 </html>
